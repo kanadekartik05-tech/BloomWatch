1cc2cc84d58aee4d231cd925bcc21f9f9124d978
Kartik Kanade <kanadekartik05@gmail.com>
2025-10-04 10:36:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1785568851-65f71",
+  "appId": "1:522518618789:web:1cc17e457e66f27746634c",
+  "apiKey": "AIzaSyBrY2BOHwcRbEcJZZrVEqSpmzO2m19sjqk",
+  "authDomain": "studio-1785568851-65f71.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "522518618789"
+};
